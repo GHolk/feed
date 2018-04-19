9fdfc24bd3179a0ab12ec14e14e98db0c4fd40d1
gholk <85619andy@gmail.com>
2018-04-19 22:29:55 +0800

    auto daily update

--- script/update-feed.sh
+++ script/update-feed.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 work_directory="/home/gholk/code/feed"
+script_list='activity-ncku.sh'
 
 cd $work_directory/script
-for shell_script in *.sh
+for shell_script in $script_list
 do
     sh -e $shell_script
 done
 
-
+git commit -am 'auto daily update'
+git push
 
