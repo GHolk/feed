a357cf76b762954aa05079b847b33138aa0e0eb5
gholk <85619andy@gmail.com>
2018-05-01 11:51:41 +0800

    use wget instead of curl
    sometimes curl do not work,
    in script mode.

--- script/activity-ncku/update.sh
+++ script/activity-ncku/update.sh
@@ -2,7 +2,7 @@
 
 url="http://activity.ncku.edu.tw/index.php?c=apply&m=read" 
 name=activity-ncku
-curl $url | iconv -f big5  >../$name.html
+wget $url -O - | iconv -f big5  >../$name.html
 node $name/update.js ../$name
 rm ../$name.html
 
