cb4da0c70af8c79498c85ad0302580f36f239aae
gholk <85619andy@gmail.com>
2018-10-21 13:56:13 +0800

    update.js write to specific path

--- script/update.js
+++ script/update.js
@@ -26,7 +26,7 @@ const anafeed = {
     async write(path) {
         if (!path) path = this.rssPath
         const content = this.feed.xml({indent: true})
-        await this.promiseWriteContent(this.rssPath, content, 'utf8')
+        await this.promiseWriteContent(path, content, 'utf8')
     },
     async loadWindow(url) {
         const dom = await this.JSDOM.fromURL(url)
@@ -54,9 +54,7 @@ const anafeed = {
             this.articleList.push(article)
         }
     },
-    parseArticle(node) {
-        throw new Error('you need specify your own parseArticle method')
-    },
+    parseArticle(node) {},
     create(option) {
         const child = Object.create(this)
         Object.assign(child, option)
