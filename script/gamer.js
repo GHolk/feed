9a1865d399300b47e67df331014b9e6409ff1236
gholk <85619andy@gmail.com>
2018-10-26 23:02:47 +0800

    usable gamer.js

--- script/gamer.js
+++ script/gamer.js
@@ -5,11 +5,18 @@ const readDirectory = util.promisify(fs.readdir)
 const readFile = util.promisify(fs.readFile)
 
 function inheritAnafeed(anafeed) {
-    const gamer = {
+    const gamerFeed = {
         __proto__: anafeed,
-        articleSelector: '.HOME-mainbox1b',
+        articleSelector: '.HOME-mainbox1',
         // rssPath: 'fe2x.rss',
         articleList: [],
+        get crawlUrl() {
+            const homePage = this.feedOption.site_url
+            return homePage.replace(/(\w+)$/, 'creation.php?owner=$1')
+        },
+        async write() {
+            console.log(this.feed.xml({indent: true}))
+        },
         async loadOptionByName(name) {
             const json = await readFile(`gamer/${name}.json`)
             const option = JSON.parse(json)
@@ -21,35 +28,59 @@ function inheritAnafeed(anafeed) {
             child.loadOptionByName(name)
             return child
         },
+        cleanDescriptionNode(node) {
+            const allAnchor = node.querySelectorAll('a')
+            if (allAnchor.length > 1) allAnchor[allAnchor.length - 1].remove()
+        },
+        parseCategory(node) {
+            const scan = node.className.match(/^IMG-(C..)$/)
+            if (scan) return this.categoryMap[scan[1]]
+            else return undefined
+        },
+        categoryMap: {
+            C07: '繪圖',
+            C08: '小說',
+            C09: '日誌',
+            C10: 'cosplay',
+            C11: '同人'
+        },
         parseArticle(node) {
-            const url = node.querySelector('h2 a').href
-            const title = node.querySelector('h2 a').textContent
+            const url = node.querySelector('a.TS1').href
+            const title = node.querySelector('a.TS1').textContent
 
             const descriptionNode =
-                  node.querySelector('.post-body').cloneNode(true)
+                  node.querySelector('p').cloneNode(true)
             this.cleanDescriptionNode(descriptionNode)
             const description = descriptionNode.textContent
 
-            const dateSelector = '.post-time time[itemprop=dateModified]'
-            const date = node.querySelector(dateSelector).dateTime
+            const metaNode = node.querySelector('.ST1')
+            const dateString = metaNode.lastChild.textContent
+            const scan = dateString.match(/^\|(\d{4}-\d\d-.*?)\|/)
+            let date
+            if (scan) date = scan[1]
 
-            const categoryNode = node.querySelector('.post-category a')
+            const categoryNode = node.querySelector('h1 img')
+            const category = this.parseCategory(categoryNode)
             const categories = []
-            if (categoryNode) categories.push(categories.textContent)
+            if (category) categories.push(category)
 
-            let imageNode = node.querySelector('.post-gallery-img img')
-            let enclosure
-            if (imageNode) {
-                enclosure = {
-                    url: imageNode.src,
-                    type: 'image'
-                }
+            let imageNode = node.querySelector('.HOME-mainbox1a img')
+            let enclosure = {
+                url: imageNode.src,
+                type: 'image'
             }
             return {url, title, description, date, enclosure, categories}
         }
     }
     const gamerList = {
-        createGamerFeed(option) {
+        gamerFeed,
+        feedList: [],
+        async addGamerFeed(name) {
+            const feed = await this.createGamerFeed(name)
+            this.feedList.push(feed)
+        },
+        async createGamerFeed(name) {
+            return await this.gamerFeed.createFeed(name)
         },
         async generateFeed() {
             // no block there, or node.js will wait until all feed load
@@ -62,7 +93,7 @@ function inheritAnafeed(anafeed) {
             return Promise.all(allWrite)
         }
     }
-    return fe2x
+    return gamerList
 }
 
 exports.inheritAnafeed = inheritAnafeed
